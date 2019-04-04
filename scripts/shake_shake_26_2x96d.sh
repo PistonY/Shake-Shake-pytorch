d37754758311236635287ad343e109f29602a655
PistonYang <pistonyang@gmail.com>
2019-04-04 18:39:11 +0800

    fix bugs

--- scripts/shake_shake_26_2x96d.sh
+++ scripts/shake_shake_26_2x96d.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python ../train.py --cutout --epochs 220 --batch-size 128 --depth 96 --width 32 --mixup --no-wd
+python ../train.py --cutout --epochs 220 --batch-size 128 --depth 26 --width 96 --mixup --no-wd

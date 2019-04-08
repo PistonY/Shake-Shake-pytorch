a4f0508ceaf91c4356638b3060f32c3dcedd09fd
PistonYang <pistonyang@gmail.com>
2019-04-08 10:15:30 +0800

    Update
    Update

--- test_module.py
+++ test_module.py
@@ -8,6 +8,6 @@ from model.ss_resnet import ShakeResNet
 from utils.summary import summary
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-model = ShakeResNet(26, 112, 10).cuda()
+model = ShakeResNet(26, 32, 10).cuda()
 # print(net(torch.randn(1, 3, 32, 32).cuda()))
 summary(model, (3, 32, 32))
